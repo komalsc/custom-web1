b03222483f70846d8e4b3d47b6efceb02b56bb42
komal <komalchauhan00022@gmail.com>
2023-02-18 09:53:59 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,84 @@
+
+import  React from 'react';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import { v4 as uuidv4 } from 'uuid';
+
+export const mydata = [{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'abhishek',
+  roll:1,
+  class:'graduate',
+  interest:'reading',
+
+  
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  class:'graduate',
+  name:'komal',
+  roll:2,
+  interest:'dancing',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'shoib',
+  roll:5,
+  class:'graduate',
+  interest:'reading',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'richa',
+  class:'graduate',
+  roll : 3,
+  interest:'singing',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'luv-kus',
+  roll:4,
+  class:'graduate',
+  interest:'religious',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'krishna',
+  roll:6,
+  class:'graduate',
+  interest:'religious',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'balram',
+  roll:7,
+  class:'graduate',
+  interest:'religious',
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'amit ranjan',
+  roll:1,
+  class:'graduate',
+  interest:'reading',
+
+  
+},
+{
+  id:uuidv4(),
+  icon:<PersonSearchIcon/>,
+  name:'anita-kumari',
+  roll:1,
+  class:'graduate-in-job',
+  interest:'reading',
+
+  
+}
+]
